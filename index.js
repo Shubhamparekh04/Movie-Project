e9545f0fce6faf8fadb8160b4e8e3ebcd6dd560a
Shubhamparekh04 <shubhamparekh04@gmail.com>
2025-04-02 13:50:12 +0530

    movies

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const port = 8089;
+const path = require("path");
 
 const app = express();
 const indexRouter = require("./routers/indexRouter");
 const db = require("./configs/database");
 app.set("view engine", "ejs");
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public"))); // Adjust the folder name if needed
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/uploads",express.static(__dirname+"/uploads"))
+
 
 app.use("/", indexRouter);
 
